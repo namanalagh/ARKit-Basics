3231e7644ac892161b8eabc4774fb31d47a514b7
Naman Alagh <namanalagh19@gmail.com>
2023-03-07 10:17:05 +0530

    .

--- ARKit Basics/ViewController.swift
+++ ARKit Basics/ViewController.swift
@@ -7,18 +7,22 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var arView: ARView!
+  
+    @IBOutlet var arView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.arView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+        self.arView.session.run(configuration)
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        // arView.scene.anchors.append(boxAnchor)
     }
 }
