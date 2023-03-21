575baa944322421596ed6c40da982b9545adcb78
Naman Alagh <namanalagh19@gmail.com>
2023-03-21 13:50:09 +0530

    .

--- ARViewController.swift
+++ ARViewController.swift
@@ -17,22 +17,17 @@ class ARViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.arView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+        //self.arView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
         self.arView.session.run(configuration)
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        
+        registerGestureRecognizers()
+        self.arView.autoenablesDefaultLighting = true
         let smokeDetScene = SCNScene(named: "Smoke Detector.dae")
         guard let currentModel = smokeDetScene?.rootNode.childNode(withName: "Smoke", recursively: true) else {
             fatalError("No model found.")
         }
         
-        currentModel.position = SCNVector3(x: 0, y: 0, z: 0)
+        currentModel.position = SCNVector3(x: 0, y: 0, z: -0.4)
         self.arView.scene.rootNode.addChildNode(currentModel)
-  
-        // Add the box anchor to the scene
-        // arView.scene.anchors.append(boxAnchor)
     }
     
     @IBAction func add(_ sender: Any) {
@@ -43,14 +38,29 @@ class ARViewController: UIViewController {
         self.arView.scene.rootNode.addChildNode(node)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func registerGestureRecognizers(){
+        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinched))
+        self.arView.addGestureRecognizer(pinchGestureRecognizer)
     }
-    */
 
+    @objc func pinched(recognizer: UIPinchGestureRecognizer){
+        if recognizer.state == .changed{
+            guard let arView = recognizer.view as? ARSCNView else{
+                return
+            }
+            
+            let touch = recognizer.location(in: arView)
+            let hitTestResults = self.arView.hitTest(touch, options: nil)
+            
+            if let hitTest = hitTestResults.first {
+                let modelNode = hitTest.node
+                let pinchScaleX = Float(recognizer.scale) * modelNode.scale.x
+                let pinchScaleY = Float(recognizer.scale) * modelNode.scale.y
+                let pinchScaleZ = Float(recognizer.scale) * modelNode.scale.z
+                
+                modelNode.scale = SCNVector3(x: pinchScaleX, y: pinchScaleY, z: pinchScaleZ)
+                recognizer.scale = 1
+            }
+        }
+    }
 }
